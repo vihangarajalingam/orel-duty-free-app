812b3832b608d049b5e5744f85b85ca62a20f913
Vihanga <vihangarajalingam7@gmail.com>
2023-09-22 01:36:35 +0530

    Implemented Winston and Morgan for logging

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
 import routes from '../routes/index.js';
+import logger from '../utils/logger.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+// Routes are defined in the routes module
 app.use('/api/v1', routes);
 
+// Using Morgan for HTTP request logging
+app.use(morgan('combined'));
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  logger.info(`Server is running on port ${PORT}`);
 });
