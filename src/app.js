d62cd4be110e8b1c2483368ec23e864c8a3a1eb4
Vihanga <vihangarajalingam7@gmail.com>
2023-09-22 03:21:38 +0530

    Added mongoose and setup mongodb connection

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 import routes from '../routes/index.js';
 import logger from '../utils/logger.js';
 
@@ -16,5 +17,17 @@ app.use(morgan('combined'));
 app.use('/api/v1', routes);
 
 app.listen(PORT, () => {
-  logger.info(`Server is running on port ${PORT}`);
+    logger.info(`Server is running on port ${PORT}`);
+});
+
+// Connect to DB
+mongoose.connect('mongodb://127.0.0.1:27017/orel', {
+    socketTimeoutMS: 0
+});
+
+mongoose.connection.on('open', () => {
+    logger.info('Database connection successful');
+});
+mongoose.connection.on('error', () => {
+    logger.error('Error occurred when connecting to database');
 });
